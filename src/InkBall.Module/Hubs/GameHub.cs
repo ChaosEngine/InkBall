de7a55e16008de832fe2fd790e39c7ab2959155d
ChaosEngine <andrzej.pauli@gmail.com>
2019-11-29 03:57:06 +0100

    Faily test: SignalR_EFCore3_MultipleInsertsOfSamePoint - doing too many inserts

--- src/InkBall.Module/Hubs/GameHub.cs
+++ src/InkBall.Module/Hubs/GameHub.cs
@@ -365,6 +365,7 @@ namespace InkBall.Module.Hubs
 				{
 					try
 					{
+int before = await _dbContext.InkBallPoint.CountAsync(p => p.iGameId == ThisGame.iId, token);
 						await _dbContext.InkBallPoint.AddAsync(db_point, token);
 						ThisGame.bIsPlayer1Active = !ThisGame.bIsPlayer1Active;
 
@@ -374,7 +375,8 @@ namespace InkBall.Module.Hubs
 // 						throw new Exception($"FAKE EXCEPTION {new_point}");
 // #endif
 						await _dbContext.SaveChangesAsync(token);
-
+int after = await _dbContext.InkBallPoint.CountAsync(p => p.iGameId == ThisGame.iId, token);
+int diff_should_be_one = after - before;
 						trans.Commit();
 					}
 					catch (Exception ex)
