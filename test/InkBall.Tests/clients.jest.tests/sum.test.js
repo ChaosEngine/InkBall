f44116e86d2481bfb743cd79881266f4723b5900
ChaosEngine <andrzej.pauli@gmail.com>
2022-08-12 19:25:52 +0200

    js-tests initial

---
+++ test/InkBall.Tests/clients.jest.tests/sum.test.js
@@ -0,0 +1,6 @@
+// const inkball = require('../../../src/InkBall.Module/IBwwwroot/js/inkball');
+import { InkBallGame } from "../../../src/InkBall.Module/IBwwwroot/js/inkball";
+
+test('inkball exists', () => {
+  expect(inkball).toBe(undefined);
+});
