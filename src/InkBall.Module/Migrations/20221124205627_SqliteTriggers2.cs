77edec391b924fcd16e32bd82c2cdb18526a747e
ChaosEngine <andrzej.pauli@gmail.com>
2023-12-13 22:09:28 +0100

    typo fix

--- src/InkBall.Module/Migrations/20221124205627_SqliteTriggers2.cs
+++ src/InkBall.Module/Migrations/20221124205627_SqliteTriggers2.cs
@@ -10,7 +10,7 @@ namespace InkBall.Module.Migrations
     {
 		protected override void Up(MigrationBuilder migrationBuilder)
 		{
-			//sadly Sqlite can drop already created drigger, 'coz when changing primary keys it WILL recreate NEW TABLES
+			//sadly Sqlite can drop already created trigger, 'coz when changing primary keys it WILL recreate NEW TABLES
 			//from scratch with data copying BUT forgets (or not implemented yet) copying triggers from old tables.
 			//so here we are doing this manually ... taken from earlier migration
 			var is_sqlite = migrationBuilder.ActiveProvider == "Microsoft.EntityFrameworkCore.Sqlite";
@@ -26,7 +26,7 @@ namespace InkBall.Module.Migrations
 
 		protected override void Down(MigrationBuilder migrationBuilder)
 		{
-            //no-op 'coz this trigger-sqlite-thing should already be applied ealier
+            //no-op 'coz this trigger-sqlite-thing should already be applied earlier
             //this is just a dummy-fix migration
 		}
     }
