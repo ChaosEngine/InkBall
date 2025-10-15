945bdcdb529175284b35c8b3fb9134a9c40534d9
ChaosEngine <andrzej.pauli@gmail.com>
2025-10-15 22:22:55 +0200

    bump: update target framework to net10.0 and upgrade package references to version 10.0.0-rc.2.25502.107
    tests: SignalR Unauthorized endpoint now returns HTTP-401
    
    commenting out Microsoft.Extensions.Identity.Stores and Microsoft.Extensions.FileProviders.Embedded <PackageReference />s
    
    bump: update EntityFrameworkCore and related package references to version 10.0.0-rc.2.25502.107
    
    bump: update packages to version 10.0.0 dotnet-10 stable
    
    bump: update SignalR and MsgPack protocol scripts to version 10.0.0
    
    bump: Npgsql.EntityFrameworkCore.PostgreSQL
    
    bump: update Oracle.EntityFrameworkCore package version to 10.23.26000

--- test/InkBall.Tests/IntegrationTests.cs
+++ test/InkBall.Tests/IntegrationTests.cs
@@ -99,9 +99,9 @@ namespace InkBall.IntegrationTests
 			}
 		}
 
-        [GeneratedRegex(@"\<input name=""__RequestVerificationToken"" type=""hidden"" value=""([^""]+)"" \/\>")]
-        private static partial Regex MyRegex();
-    }
+		[GeneratedRegex(@"\<input name=""__RequestVerificationToken"" type=""hidden"" value=""([^""]+)"" \/\>")]
+		private static partial Regex MyRegex();
+	}
 
 	[Collection(nameof(TestingServerCollection))]
 	public class UnAuthenticated
@@ -170,8 +170,7 @@ namespace InkBall.IntegrationTests
 		[InlineData("InkBall/Game")]
 		[InlineData("InkBall/GamesList")]
 		[InlineData("InkBall/Highscores")]
-		[InlineData(InkBall.Module.Hubs.GameHub.HubName)]
-		public async Task Pages_Unauthorized(string page)
+		public async Task Pages_OkUnauthorized(string page)
 		{
 			//if (_fixture.DOTNET_RUNNING_IN_CONTAINER) return;//pass on fake DB with no data
 
@@ -180,8 +179,24 @@ namespace InkBall.IntegrationTests
 			using (var response = await _anonclient.GetAsync($"{_anonclient.BaseAddress}{page}"))
 			{
 				// Assert
-				//Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
 				Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+				Assert.Equal($"{_anonclient.BaseAddress}Identity/Account/Login?ReturnUrl=%2F{UrlEncoder.Default.Encode(page)}",
+					response.Headers.Location.ToString());
+			}
+		}
+
+		[Theory]
+		[InlineData(Module.Hubs.GameHub.HubName)]
+		public async Task Pages_BadUnauthorized(string page)
+		{
+			//if (_fixture.DOTNET_RUNNING_IN_CONTAINER) return;//pass on fake DB with no data
+
+			// Arrange
+			//Act
+			using (var response = await _anonclient.GetAsync($"{_anonclient.BaseAddress}{page}"))
+			{
+				// Assert
+				Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
 				Assert.Equal($"{_anonclient.BaseAddress}Identity/Account/Login?ReturnUrl=%2F{UrlEncoder.Default.Encode(page)}",
 					response.Headers.Location.ToString());
 			}
@@ -378,7 +393,7 @@ namespace InkBall.IntegrationTests
 
 			using (var request = new HttpRequestMessage(HttpMethod.Get, $"{client.BaseAddress}InkBall/Home"))
 			{
-			
+
 				// Act
 				using (var get_response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead))
 				{
@@ -443,7 +458,7 @@ namespace InkBall.IntegrationTests
 			}//end using request
 		}
 
-        [GeneratedRegex(@"iGameID\: ([0-9].*),")]
-        private static partial Regex MyRegex();
-    }
+		[GeneratedRegex(@"iGameID\: ([0-9].*),")]
+		private static partial Regex MyRegex();
+	}
 }
